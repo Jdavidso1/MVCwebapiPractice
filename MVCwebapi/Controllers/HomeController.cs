8a2f69642b59f9f8d94c68d42c8f487a8e97517a
Jdavidso1 <jdavidso1@hotmail.com>
2021-06-30 14:59:57 -0400

    added content result return method

--- MVCwebapi/Controllers/HomeController.cs
+++ MVCwebapi/Controllers/HomeController.cs
@@ -4,11 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace MVCwebapi.Controllers
+namespace MVCtutorial.Controllers
 {
     public class HomeController : Controller
     {
-
+        // GET: Home
         public ActionResult Index()
         {
             return View();
@@ -16,12 +16,31 @@ namespace MVCwebapi.Controllers
 
         public ActionResult Products()
         {
-            return View();
+            return View("OurCompanyProducts");
         }
 
         public ActionResult Contact()
         {
             return View();
+        }
+
+        public ActionResult GetEmpName(int EmpId)
+        {
+            var employees = new[] {
+            new { EmpId = 1, EmpName = "Scott", Salary = 8000 },
+            new { EmpId = 2, EmpName = "Paul", Salary = 6500 },
+            new { EmpId = 3, EmpName = "Eric", Salary = 9500 }
+        };
+            string matchEmpName = null;
+            foreach (var item in employees)
+            {
+                if (item.EmpId == EmpId)
+                {
+                    matchEmpName = item.EmpName;
+                }
+            }
+            //return new ContentResult() { Content = matchEmpName, ContentType = "text/plain" };
+            return Content(matchEmpName, "text/plain");
         }
     }
 }
